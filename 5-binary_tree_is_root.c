acf4eb46852e3805f8d3e6a3ec0646a8cc4ffcc9
Cappu123 <cappyalemayehu12@gmail.com>
2024-02-29 13:50:33 -0800

    is root

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -7,10 +7,11 @@
  * Return: 1 if node is a root, 0 otherwise
  *
  */
+
 int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (node == NULL || node->parent != NULL)
 		return (0);
-
-		return (1);
+	
+	return (1);
 }
