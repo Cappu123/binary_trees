bd909e0791d81325ef669a2c83bc108a4c3bf0f6
Cappu123 <cappyalemayehu12@gmail.com>
2024-02-29 11:59:41 -0800

    binary_tree_insert_left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -14,9 +14,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	{
 		return (NULL);
 	}
-	
 	newNode = binary_tree_node(parent, value);
-
 	if (newNode == NULL)
 	{
 		return (NULL);
